d41eee52820aac6384c00c3c6c65ea19edb8fc68
Richard <97440893+z1ku@users.noreply.github.com>
2022-11-07 10:03:54 +0100

    empezando

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <img src="img/logo.png" alt="">
+        <nav>
+            <a href="">Socios</a>
+            <a href="">Productos</a>
+            <a href="">Servicios</a>
+            <a href="">Testimonios</a>
+            <a href="">Noticias</a>
+            <a href="">Citas</a>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <h1>Bienvenido a GoFit</h1>  
+        </section>
+        <section>
+            <h2>Últimas noticias</h2>
+            <?php
+                //para sacar las 3 ultimas noticias
+                //select * from noticia where .... limit 3,2
+
+            ?>
+        </section>
+        <section>
+            <h2>Testimonios</h2>
+            <?php
+                require_once "php/funciones.php";
+
+                //mt_rand(inf, sup)
+                
+            ?>
+        </section>
+        <section>
+            <h2>Contacto</h2>
+            <form action="" method="post">
+                <div>
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" name="nombre">
+                </div>
+                <div>
+                    <label for="correo">Correo:</label>
+                    <input type="email" name="correo">
+                </div>
+                <label for="mensaje">Mensaje:</label>
+                <textarea name="mensaje" cols="30" rows="10"></textarea>
+                <input type="submit" name="enviar">
+            </form>
+        </section>
+    </main>
+    <footer>
+        <span>DESARROLLADO POR RICARDO ROMERO BUSTOS</span>
+    </footer>
+</body>
+</html>
---
+++ php/funciones.php
@@ -0,0 +1,12 @@
+<?php
+    //cerrar la conexion?
+    function conectarServidor(){
+        $con=new mysqli('localhost', 'root', '', 'club');
+        $con->set_charset('utf8');
+
+        return $con;
+    }
+    
+
+
+?>
