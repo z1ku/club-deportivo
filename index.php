79235936c9f9f69f3b688b0cec6464bc2ebbe2a1
Richard <97440893+z1ku@users.noreply.github.com>
2022-11-08 12:40:25 +0100

    menu y footer

--- index.php
+++ index.php
@@ -6,11 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="css/estilos.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Play&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <header>
-        <img src="img/logo.png" alt="" id="logo">
         <nav>
+            <a href="index.php"><img src="img/logo.png" alt="" id="logo"></a>
             <a href="">Socios</a>
             <a href="">Productos</a>
             <a href="">Servicios</a>
@@ -18,6 +22,24 @@
             <a href="">Noticias</a>
             <a href="">Citas</a>
         </nav>
+        <div class="rrss">
+            <a href=""><i class="fa-brands fa-twitter"></i></a>
+            <a href=""><i class="fa-brands fa-facebook"></i></a>
+            <a href=""><i class="fa-brands fa-youtube"></i></a>
+        </div>
+        <div class="login">
+            <form action="" method="post">
+                <div>
+                    <label for="usuario">Usuario:</label>
+                    <input type="text" name="usuario">
+                </div>
+                <div>
+                    <label for="pass">Contraseña:</label>
+                    <input type="password" name="pass">
+                </div>
+                <input type="submit" name="enviar" id="btn-login">
+            </form>
+        </div>
     </header>
     <main>
         <section>
@@ -70,6 +92,11 @@
         </section>
     </main>
     <footer>
+        <div>
+            <a href="">Política de privacidad</a>
+            <a href="">Condiciones</a>
+            <a href="">Contacto</a>
+        </div>
         <span>DESARROLLADO POR RICARDO ROMERO BUSTOS</span>
     </footer>
 </body>
