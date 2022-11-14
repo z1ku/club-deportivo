054d26f4ad80b7becafdb4ace1af343ab0241129
Richard <z1ku1337@gmail.com>
2022-11-14 10:50:43 +0100

    testimonios acabado

--- php/editar_testimonio.php
+++ php/editar_testimonio.php
@@ -1,6 +1,25 @@
 <?php
     if(isset($_POST['insertar_testimonio'])){
+        require_once "funciones.php";
+
+        $autor=$_POST['autor'];
+        $contenido=$_POST['contenido'];
+        $fecha=date('Y-m-d');
+
+        $con=conectarServidor();
+
+        $insertar=$con->prepare("insert into testimonio values(null,?,?,?)");
+        $insertar->bind_param("sss",$autor,$contenido,$fecha);
+
+        if($insertar->execute()){
+            echo "<p>Testimonio nuevo insertado correctamente</p>";
+        }else{
+            echo "<p>ERROR:</p> " . $insertar->error;
+        }
         
+        $insertar->close();
+        $con->close();
+        header("refresh:2; url=testimonios.php");
     }else{
         header("Location:testimonios.php");
     }
