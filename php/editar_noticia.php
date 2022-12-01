39a1507a7a54da08db46a1c0285ba5a4c6a23066
Richard <z1ku1337@gmail.com>
2022-12-01 13:55:13 +0100

    validar noticias

--- php/editar_noticia.php
+++ php/editar_noticia.php
@@ -1,43 +1,52 @@
 <?php
     if(isset($_POST['insertar_noticia'])){
-        $fecha=$_POST['fecha'];
-        $marca=strtotime($fecha);
-        $dia=date('d', $marca);
-        $mes=date('m', $marca);
-        $anio=date('Y', $marca);
 
-        $fecha_actual=date('Y-m-d');
-
-        if(!checkdate($mes, $dia, $anio)){
-            echo "<p>Fecha incorrecta</p>";
-            header("refresh:2; url=noticias.php");
-        }
-
-        if($_FILES['imagen']['type']!="image/jpeg"){
-            echo "<p>La imagen no es un jpg</p>";
-        }else if($fecha<$fecha_actual){
-            echo "<p>La fecha no puede ser anterior a hoy</p>";
+        if($_POST['titulo']=="" || $_POST['contenido']=="" || $_POST['fecha']=="" || !is_uploaded_file($_FILES['foto']['tmp_name'])){
+            echo "<p>Debes de rellenar todos los campos y subir una foto jpeg</p>";
+        }else if(strlen($_POST['titulo'])>80){
+            echo "<p>Titulo no puede ser mayor de 80</p>";
+        }else if(strllen($_POST['contenido'])>800){
+            echo "<p>Contenido no puede ser mayor de 800</p>";
         }else{
-            require_once "funciones.php";
+            $fecha=$_POST['fecha'];
+            $marca=strtotime($fecha);
+            $dia=date('d', $marca);
+            $mes=date('m', $marca);
+            $anio=date('Y', $marca);
 
-            $con=conectarServidor();
+            $fecha_actual=date('Y-m-d');
 
-            $titulo=$_POST['titulo'];
-            $contenido=$_POST['contenido'];
-            $imagen=$_POST['id'].".jpg";
-
-            $insertar=$con->prepare("insert into noticia values(null,?,?,?,?)");
-            $insertar->bind_param("ssss", $titulo,$contenido,$imagen,$fecha);
-
-            if($insertar->execute()){
-                move_uploaded_file($_FILES['imagen']['tmp_name'], "../img/noticias/$imagen");
-                echo "<p>Noticia nueva insertada correctamente</p>";
-            }else{
-                echo "<p>ERROR:</p> " . $insertar->error;
+            if(!checkdate($mes, $dia, $anio)){
+                echo "<p>Fecha incorrecta</p>";
+                header("refresh:2; url=noticias.php");
             }
 
-            $insertar->close();
-            $con->close();
+            if($_FILES['imagen']['type']!="image/jpeg"){
+                echo "<p>La imagen no es un jpg</p>";
+            }else if($fecha<$fecha_actual){
+                echo "<p>La fecha no puede ser anterior a hoy</p>";
+            }else{
+                require_once "funciones.php";
+
+                $con=conectarServidor();
+
+                $titulo=$_POST['titulo'];
+                $contenido=$_POST['contenido'];
+                $imagen=$_POST['id'].".jpg";
+
+                $insertar=$con->prepare("insert into noticia values(null,?,?,?,?)");
+                $insertar->bind_param("ssss", $titulo,$contenido,$imagen,$fecha);
+
+                if($insertar->execute()){
+                    move_uploaded_file($_FILES['imagen']['tmp_name'], "../img/noticias/$imagen");
+                    echo "<p>Noticia nueva insertada correctamente</p>";
+                }else{
+                    echo "<p>ERROR:</p> " . $insertar->error;
+                }
+
+                $insertar->close();
+                $con->close();
+            }
         }
         
         header("refresh:2; url=noticias.php");
