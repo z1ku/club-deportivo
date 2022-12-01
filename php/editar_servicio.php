77180229f09b0090cce48db5b84e0bd15ff9ae3c
Richard <z1ku1337@gmail.com>
2022-12-01 13:32:53 +0100

    validar servicios

--- php/editar_servicio.php
+++ php/editar_servicio.php
@@ -1,45 +1,67 @@
 <?php
     if(isset($_POST['editar_servicio'])){
-        require_once "funciones.php";
-
-        $id=$_POST['id'];
-        $descripcion=$_POST['descripcion'];
-        $duracion=$_POST['duracion'];
-        $precio=$_POST['precio'];
-
-        $con=conectarServidor();
-        $sentencia=$con->prepare("update servicio set descripcion=?, duracion=?, precio=? where id=$id");
-        $sentencia->bind_param("sid", $descripcion,$duracion,$precio);
-
-        if($sentencia->execute()){
-            echo "<p>Servicio editado correctamente</p>";
+        if($_POST['descripcion']=="" || $_POST['duracion']=="" || $_POST['precio']==""){
+            echo "<p>No puede haber campos vacíos</p>";
+        }else if(strlen($_POST['descripcion'])>80){
+            echo "<p>Descripción no puede ser mayor a 80</p>";
+        }else if($_POST['duracion']<0 || $_POST['duracion']>999){
+            echo "<p>Duración no válida</p>";
+        }else if($_POST['precio']<0 || $_POST['precio']>999.99){
+            echo "<p>Precio no válido</p>";
         }else{
-            echo "<p>ERROR:</p> " . $sentencia->error;
+            require_once "funciones.php";
+
+            $id=$_POST['id'];
+            $descripcion=$_POST['descripcion'];
+            $duracion=$_POST['duracion'];
+            $precio=$_POST['precio'];
+
+            $con=conectarServidor();
+            $sentencia=$con->prepare("update servicio set descripcion=?, duracion=?, precio=? where id=$id");
+            $sentencia->bind_param("sid", $descripcion,$duracion,$precio);
+
+            if($sentencia->execute()){
+                echo "<p>Servicio editado correctamente</p>";
+            }else{
+                echo "<p>ERROR:</p> " . $sentencia->error;
+            }
+            
+            $sentencia->close();
+            $con->close();
         }
-        
-        $sentencia->close();
-        $con->close();
+
         header("refresh:2; url=servicios.php");
     }else if(isset($_POST['insertar_servicio'])){
-        require_once "funciones.php";
-
-        $descripcion=$_POST['descripcion'];
-        $duracion=$_POST['duracion'];
-        $precio=$_POST['precio'];
-
-        $con=conectarServidor();
-
-        $insertar=$con->prepare("insert into servicio values(null,?,?,?)");
-        $insertar->bind_param("sid", $descripcion,$duracion,$precio);
-
-        if($insertar->execute()){
-            echo "<p>Servicio nuevo insertado correctamente</p>";
+        if($_POST['descripcion']=="" || $_POST['duracion']=="" || $_POST['precio']==""){
+            echo "<p>No puede haber campos vacíos</p>";
+        }else if(strlen($_POST['descripcion'])>80){
+            echo "<p>Descripción no puede ser mayor a 80</p>";
+        }else if($_POST['duracion']<0 || $_POST['duracion']>999){
+            echo "<p>Duración no válida</p>";
+        }else if($_POST['precio']<0 || $_POST['precio']>999.99){
+            echo "<p>Precio no válido</p>";
         }else{
-            echo "<p>ERROR:</p> " . $insertar->error;
-        }
+            require_once "funciones.php";
 
-        $insertar->close();
-        $con->close();
+            $descripcion=$_POST['descripcion'];
+            $duracion=$_POST['duracion'];
+            $precio=$_POST['precio'];
+
+            $con=conectarServidor();
+
+            $insertar=$con->prepare("insert into servicio values(null,?,?,?)");
+            $insertar->bind_param("sid", $descripcion,$duracion,$precio);
+
+            if($insertar->execute()){
+                echo "<p>Servicio nuevo insertado correctamente</p>";
+            }else{
+                echo "<p>ERROR:</p> " . $insertar->error;
+            }
+
+            $insertar->close();
+            $con->close();
+        }
+        
         header("refresh:2; url=servicios.php");
     }else{
         header("Location:servicios.php");
